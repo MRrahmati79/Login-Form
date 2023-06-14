7906c96f35738eb5600c0bd4a088ca4a9855f078
mr.rahmati <mr.rahmati.9105@gmail.com>
2023-06-14 20:33:20 +0330

    Let's go for a few hours rest

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let userName = document.querySelector('.userName')
+let passWord = document.querySelector('.passWord')
+let userMesage = document.getElementById('userMesage')
+let pasMesage = document.getElementById('pasMesage')
+
+
+
+function mmm () {
+    let valueUser = userName.value  
+    let valuePass = passWord.value  
+
+
+    if(valuePass){}
+
+
+}
